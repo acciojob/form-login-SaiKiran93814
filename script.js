6c5e10ef6e6b8cf514856cdbcb8198a6bf441eb0
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-15 11:46:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+	const form= document.forms[0];
+	const fname=form.fname.value.trim();
+	const lname=form.lname.value.trim();
+
+	alert('${fname} ${lname}');
 
 }
